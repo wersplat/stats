6887e17f107d86a8d9523aa52a97287d6030409a
Wersplat <wersplat@users.noreply.github.com>
2025-07-18 04:16:26 -0400

    chore: install project dependencies and node modules

---
+++ build.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+// Get list of all recap files
+const recaps = fs.readdirSync('.')
+    .filter(file => file.endsWith('_recap.txt'));
+
+// Read the worker file
+let workerContent = fs.readFileSync('_worker.js', 'utf8');
+
+// Update the RECAP_FILES array in the worker file
+workerContent = workerContent.replace(
+    'const RECAP_FILES = [\n    // This will be replaced by the build script\n];',
+    `const RECAP_FILES = ${JSON.stringify(recaps, null, 4)};`
+);
+
+// Write the updated worker file
+fs.writeFileSync('_worker.js', workerContent);
+
+console.log('Build completed. Found', recaps.length, 'recap files.');
